e20de5be95293fd4ce47501681788f117dcf5882
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-07-21 00:09:25 +0300

    dynamically styling with ngStyle

--- angular-guide/src/app/server/server.component.ts
+++ angular-guide/src/app/server/server.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
 })
-
 export class ServerComponent {
   serverID = 10;
-  serverStatus = 'offline'
+  serverStatus: string;
 
-  getServerStatus(){
-    return this.serverStatus
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
