36f25bdb916a0a5876ba260fd43a3cc1865d7cd6
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-08-01 23:58:43 +0300

    initial commit

--- angular-guide/src/app/app.component.ts
+++ angular-guide/src/app/app.component.ts
@@ -3,14 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.scss']
-  styles: [
-    `
-      h3 {
-        color: dodgerblue;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-guide';
--- angular-guide/src/app/app.module.ts
+++ angular-guide/src/app/app.module.ts
@@ -3,20 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
