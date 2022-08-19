cdc43327059157e584bb169e64ba55729866c84e
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-08-19 22:31:34 +0300

    contentChild

--- angular-guide/src/app/server-element/server-element.component.ts
+++ angular-guide/src/app/server-element/server-element.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 export type ServerType = { name: string; type: string; content: string };
 
@@ -7,12 +16,18 @@ export type ServerType = { name: string; type: string; content: string };
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.scss'],
   // encapsulation: ViewEncapsulation.None
-  encapsulation: ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.Emulated,
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterContentInit {
   @Input('srvElement') element: ServerType;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() {}
-
+  
+  ngAfterContentInit(): void {
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
+  
   ngOnInit(): void {}
+
 }
