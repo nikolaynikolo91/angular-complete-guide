85ae53d3cd875bc9ee7eb2fe5747ba0d59b8ca16
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-08-19 23:14:54 +0300

    adding navigation with event binding and ngif

--- angular-guide/src/app/app.component.ts
+++ angular-guide/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-guide';
+  navigateTo = '';
+
+  onNavigate(feature: string) {
+    this.navigateTo = feature;
+  }
 }
