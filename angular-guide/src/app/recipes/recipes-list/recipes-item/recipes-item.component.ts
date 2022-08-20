626098b8af5b864fb67e011f274e6f6fa04f58af
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-08-20 20:20:00 +0300

    passing data with event and property bindin (combined)

--- angular-guide/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ angular-guide/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipes-item',
   templateUrl: './recipes-item.component.html',
-  styleUrls: ['./recipes-item.component.scss']
+  styleUrls: ['./recipes-item.component.scss'],
 })
 export class RecipesItemComponent implements OnInit {
-  @Input() recipe:Recipe;
-  
-  constructor() { }
+  @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelected() {
+    this.recipeSelected.emit();
   }
-
 }
--- angular-guide/src/app/recipes/recipes-list/recipes-list.component.ts
+++ angular-guide/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -14,13 +14,19 @@ export class RecipesListComponent implements OnInit {
       'https://www.1001recepti.com/images/photos/recipes/size_5/agneshka-shkembe-chorba-s-priasno-mliako-cherven-piper-chesun-i-ocet-19ad671f19a315487e130c8571bacdc6-[104174].jpg'
     ),
     new Recipe(
-      'A Test Recipe',
+      'A Test Recipe2',
       'This is simply a test2',
       'https://www.1001recepti.com/images/photos/recipes/size_5/agneshka-shkembe-chorba-s-priasno-mliako-cherven-piper-chesun-i-ocet-19ad671f19a315487e130c8571bacdc6-[104174].jpg'
     ),
   ];
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeClicked(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
