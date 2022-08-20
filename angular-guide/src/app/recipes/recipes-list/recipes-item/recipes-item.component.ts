e45cb08fd479dc69a305af6cdb9f72cc5e7151d8
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-08-20 19:40:41 +0300

    passing recipe data with property binding

--- angular-guide/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ angular-guide/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipes-item',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-item.component.scss']
 })
 export class RecipesItemComponent implements OnInit {
-
+  @Input() recipe:Recipe;
+  
   constructor() { }
 
   ngOnInit(): void {
