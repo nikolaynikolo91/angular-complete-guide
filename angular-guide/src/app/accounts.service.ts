335f59a785e7b67a11b7b27f92c822e6bba17b1d
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-08-21 19:57:13 +0300

    creating a data service

---
+++ angular-guide/src/app/accounts.service.ts
@@ -0,0 +1,29 @@
+export type AccountServer = {
+  name: string;
+  status: string;
+};
+
+export class AccountsService {
+  accounts: AccountServer[] = [
+    {
+      name: 'Master Account',
+      status: 'active',
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive',
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown',
+    },
+  ];
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({ name, status });
+  }
+
+  updateStatus(id: number, status: string) {
+    this.accounts[id].status = status;
+  }
+}
