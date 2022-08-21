fb6f424eeba06407b4d073cfb71eb3fac65f2ab4
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-08-21 18:52:26 +0300

    closing the dropdown from anywhere

--- angular-guide/src/app/shared/dropdown.directives.ts
+++ angular-guide/src/app/shared/dropdown.directives.ts
@@ -1,5 +1,6 @@
 import {
   Directive,
+  ElementRef,
   HostBinding,
   HostListener,
 } from '@angular/core';
@@ -8,7 +9,11 @@ import {
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    // this.isOpen = !this.isOpen;
+
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+
+  constructor(private elRef: ElementRef) {}
 }
