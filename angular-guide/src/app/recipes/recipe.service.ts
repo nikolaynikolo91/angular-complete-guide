79a2e5ae13b6851b29f6c53ec2a5526bb40b5175
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-08-21 23:46:46 +0300

    managing recipes in a recipe service

---
+++ angular-guide/src/app/recipes/recipe.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  constructor() {}
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.1001recepti.com/images/photos/recipes/size_5/agneshka-shkembe-chorba-s-priasno-mliako-cherven-piper-chesun-i-ocet-19ad671f19a315487e130c8571bacdc6-[104174].jpg'
+    ),
+    new Recipe(
+      'A Test Recipe2',
+      'This is simply a test2',
+      'https://www.1001recepti.com/images/photos/recipes/size_5/agneshka-shkembe-chorba-s-priasno-mliako-cherven-piper-chesun-i-ocet-19ad671f19a315487e130c8571bacdc6-[104174].jpg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
--- angular-guide/src/app/recipes/recipes-list/recipes-list.component.ts
+++ angular-guide/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,24 +8,15 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.scss'],
 })
 export class RecipesListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://www.1001recepti.com/images/photos/recipes/size_5/agneshka-shkembe-chorba-s-priasno-mliako-cherven-piper-chesun-i-ocet-19ad671f19a315487e130c8571bacdc6-[104174].jpg'
-    ),
-    new Recipe(
-      'A Test Recipe2',
-      'This is simply a test2',
-      'https://www.1001recepti.com/images/photos/recipes/size_5/agneshka-shkembe-chorba-s-priasno-mliako-cherven-piper-chesun-i-ocet-19ad671f19a315487e130c8571bacdc6-[104174].jpg'
-    ),
-  ];
+  recipes: Recipe[];
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeClicked(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
