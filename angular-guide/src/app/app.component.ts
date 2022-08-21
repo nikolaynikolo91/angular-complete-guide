4d8ebc3d91d94db895c58a3b32171c27c2a18f5d
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-08-21 19:00:50 +0300

    initial commit

--- angular-guide/src/app/app.component.ts
+++ angular-guide/src/app/app.component.ts
@@ -3,13 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-guide';
-  navigateTo = '';
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
 
-  onNavigate(feature: string) {
-    this.navigateTo = feature;
+  onAccountAdded(newAccount: {name: string, status: string}) {
+    this.accounts.push(newAccount);
+  }
+
+  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+    this.accounts[updateInfo.id].status = updateInfo.newStatus;
   }
 }
