737054df32075ec7f22b73066a400e849c307231
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-09-07 11:57:45 +0300

    adding the shopping list service

--- angular-guide/src/app/shopping-list/shopping-list.service.ts
+++ angular-guide/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingListService {
+  constructor() {}
 
-  constructor() { }
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
