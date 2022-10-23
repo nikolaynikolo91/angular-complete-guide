d5de977b1329519a8a0bbb7c42949453d8275659
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-10-23 22:00:49 +0300

    custom interval

--- angular-guide/src/app/home/home.component.ts
+++ angular-guide/src/app/home/home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subject, Subscription, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  // private firstObsSubscription: Subscription;
+  private destroy = new Subject();
+  constructor() {}
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => console.log(count))
+    // interval(1000)
+    //   .pipe(takeUntil(this.destroy))
+    //   .subscribe((count) => console.log(count));
+
+    const customIntervalObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    customIntervalObservable
+      .pipe(takeUntil(this.destroy))
+      .subscribe((data) => console.log(data));
   }
 
+  ngOnDestroy(): void {
+    // this.firstObsSubscription.unsubscribe()
+    this.destroy.next('');
+    this.destroy.complete();
+  }
 }
