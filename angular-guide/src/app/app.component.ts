d9f527df52ca1419bdfe158ac934a13d783bb218
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-10-23 22:56:24 +0300

    subjects

--- angular-guide/src/app/app.component.ts
+++ angular-guide/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  isActivated = false;
 
-  ngOnInit() {}
+  constructor(private service: UserService) {}
+
+  ngOnInit() {
+    this.service.activatedEmitter.subscribe((didActivated) => {
+      this.isActivated = didActivated;
+    });
+  }
 }
