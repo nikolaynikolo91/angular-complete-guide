bca480acc85e038aaeca3635c34c2c8433e98935
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-10-24 23:16:24 +0300

    TD: Handling radion buttons

--- angular-guide/src/app/app.component.ts
+++ angular-guide/src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   @ViewChild('f') onSignUpForm: NgForm;
   defaultQuestion = 'pet';
   answer: string;
-
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
