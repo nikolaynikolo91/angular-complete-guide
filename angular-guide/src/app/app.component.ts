fe0386dedef90c87f9d78f6d64f09dc13b16687d
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-10-26 00:16:35 +0300

    adding FormArray

--- angular-guide/src/app/app.component.ts
+++ angular-guide/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -17,10 +17,20 @@ export class AppComponent implements OnInit {
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl('male'),
+      hobbies: new FormArray([new FormControl(null, Validators.required)]),
     });
   }
 
   onSubmit() {
     console.log(this.signupForm.value);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  get hobbiesControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
   }
 }
