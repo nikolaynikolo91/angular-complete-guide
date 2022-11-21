e4c0fe1bfa9bdb483208e103be73c780cc95ebd2
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-11-21 11:59:44 +0200

    adding new ingredient controls

--- angular-guide/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ angular-guide/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -62,7 +62,17 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
-  get controls() { // a getter!
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl(),
+      })
+    );
+  }
+
+  get controls() {
+    // a getter!
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
 }
