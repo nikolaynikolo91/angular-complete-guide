1dccb7d20a678406436963bff326deba3b50715a
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-12-15 14:45:25 +0200

    add interceptors

---
+++ angular-guide/src/app/auth-interceptor.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    console.log('Request is on the way');
+    return next.handle(req);
+  }
+}
