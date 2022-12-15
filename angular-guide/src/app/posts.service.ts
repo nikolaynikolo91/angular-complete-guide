9ef167716f2c0ce6acb47bd0aa4b097039988d41
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-12-15 11:47:38 +0200

    adding query params

--- angular-guide/src/app/posts.service.ts
+++ angular-guide/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, catchError } from 'rxjs';
 import { Post } from './post.model';
@@ -22,9 +22,16 @@ export class PostsService {
   }
 
   fetchPosts() {
+    //  multiple params
+    // let searchParams = new HttpParams();
+    // searchParams = searchParams.append('print', 'pretty');
+    // searchParams = searchParams.append('custom', 'key');
+
     return this.http
       .get<{ [key: string]: Post }>(BASE_URL + POSTS + URL_END, {
         headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+        params: new HttpParams().set('print', 'pretty')
+        // params: searchParams
       })
       .pipe(
         map((resData) => {
