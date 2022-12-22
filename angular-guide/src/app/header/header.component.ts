3750356d7f3d1052b82c51d6449498f10bc4b097
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2022-12-22 15:34:08 +0200

    fetching recipes

--- angular-guide/src/app/header/header.component.ts
+++ angular-guide/src/app/header/header.component.ts
@@ -17,4 +17,8 @@ export class HeaderComponent {
   onSaveData() {
     this.dataStoreService.storeRecipes();
   }
+
+  onFetchData() {
+    this.dataStoreService.fetchRecipesData();
+  }
 }
--- angular-guide/src/app/shared/data-storage.service.ts
+++ angular-guide/src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
@@ -15,6 +16,17 @@ export class DataStorageService {
       )
       .subscribe((recipes) => {
         console.log(recipes);
+      });
+  }
+
+  fetchRecipesData() {
+    this.http
+      .get<Recipe[]>(
+        'https://ng-guilde-complete-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+      )
+      .subscribe((recipes: Recipe[]) => {
+        console.log(recipes);
+        this.recipeService.setRecipes(recipes);
       });
   }
 }
