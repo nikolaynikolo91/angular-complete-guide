0393232b2cdfe84fe60aefbeee1f70e888ae1dc3
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2023-01-16 22:53:17 +0200

    reflecting the auth state in the ui

--- angular-guide/src/app/auth/auth.component.ts
+++ angular-guide/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthResponseData, AuthService } from './auth.service';
 
@@ -12,7 +13,7 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
 
@@ -40,6 +41,7 @@ export class AuthComponent implements OnInit {
       next: (response) => {
         console.log(response, 'Response');
         this.isLoading = false;
+        this.router.navigate(['/recipes']);
       },
       error: (errorMassage) => {
         this.error = errorMassage;
--- angular-guide/src/app/header/header.component.ts
+++ angular-guide/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
@@ -6,13 +8,20 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  // @Output() featureSelected = new EventEmitter<string>();
-  // onSelect(feat: string) {
-  //   this.featureSelected.emit(feat);
-  // }
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
 
-  constructor(private dataStoreService: DataStorageService) {}
+  private destroy = new Subject<void>();
+  constructor(
+    private dataStoreService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.user.pipe(takeUntil(this.destroy)).subscribe((user) => {
+      this.isAuthenticated = !!user;
+    });
+  }
 
   onSaveData() {
     this.dataStoreService.storeRecipes();
@@ -20,5 +29,10 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStoreService.fetchRecipesData().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
   }
 }
