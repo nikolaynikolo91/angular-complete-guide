56b7d1fe4e33ad7ce6b82c75dfc98a297d42e597
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2023-01-16 18:17:17 +0200

    login error handling

--- angular-guide/src/app/auth/auth.service.ts
+++ angular-guide/src/app/auth/auth.service.ts
@@ -1,11 +1,17 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs';
+import { catchError, throwError } from 'rxjs';
 
 const API_KEY = 'AIzaSyDKk_QQL8MmKqZbSePc9r0sSatToO6ABa0';
+const SIGNUP_URL =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
+const SIGNIN_URL =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
 
 const ERROR_MESSAGES: { [key: string]: string } = {
   EMAIL_EXISTS: 'This email exist already',
+  EMAIL_NOT_FOUND: 'This email does not exist.',
+  INVALID_PASSWORD: 'This password is not correct.',
 };
 
 export interface AuthResponseData {
@@ -23,43 +29,28 @@ export class AuthService {
 
   signup(email: string, password: string) {
     return this.http
-      .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
-          API_KEY,
-        {
-          email,
-          password,
-          returnSecureToken: true,
-        }
-      )
-      .pipe(
-        catchError(({ error }) => {
-          const message = ERROR_MESSAGES[error.error.message];
-          if (message) {
-            throw message;
-          } else throw 'An unkown error occurd!';
-        })
-      );
+      .post<AuthResponseData>(SIGNUP_URL + API_KEY, {
+        email,
+        password,
+        returnSecureToken: true,
+      })
+      .pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string) {
     return this.http
-      .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
-          API_KEY,
-        {
-          email,
-          password,
-          returnSecureToken: true,
-        }
-      )
-      .pipe(
-        catchError(({ error }) => {
-          const message = ERROR_MESSAGES[error.error.message];
-          if (message) {
-            throw message;
-          } else throw 'An unkown error occurd!';
-        })
-      );
+      .post<AuthResponseData>(SIGNIN_URL + API_KEY, {
+        email,
+        password,
+        returnSecureToken: true,
+      })
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError({ error }: HttpErrorResponse) {
+    const message = ERROR_MESSAGES[error.error.message];
+    if (message) {
+      return throwError(() => new Error(message));
+    } else return throwError(() => new Error('An unkown error occurd!'));
   }
 }
