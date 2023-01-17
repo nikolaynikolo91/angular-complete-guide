9f0550a0e2c76e24fdbd37c15755b0e35c66fd2c
Nikolay Nikolov <nikolaynikolov91@gmail.com>
2023-01-18 00:48:42 +0200

    adding ana auth guard

--- angular-guide/src/app/app.route.module.ts
+++ angular-guide/src/app/app.route.module.ts
@@ -6,11 +6,20 @@ export const appRoutes: Routes = [
   {
     path: 'recipes',
     component: RecipesComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: '', component: RecipeStartComponent },
       { path: 'new', component: RecipeEditComponent },
-      { path: ':id', component: RecipesDetailComponent , resolve: [RecipesResolverService]},
-      { path: ':id/edit', component: RecipeEditComponent , resolve: [RecipesResolverService]},
+      {
+        path: ':id',
+        component: RecipesDetailComponent,
+        resolve: [RecipesResolverService],
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+        resolve: [RecipesResolverService],
+      },
     ],
   },
   { path: 'shopping-list', component: ShoppingListComponent },
@@ -24,6 +33,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
