012a70182387297168abaa0566b57f07a2ef5ca7
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2023-03-06 17:00:24 +0200

    add more lazy loading

--- angular-guide/src/app/app.module.ts
+++ angular-guide/src/app/app.module.ts
@@ -4,11 +4,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRouterModule } from './app.route.module';
 import { HttpClientModule } from '@angular/common/http';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -16,8 +13,6 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     HttpClientModule,
     AppRouterModule,
-    ShoppingListModule,
-    AuthModule,
     SharedModule,
     CoreModule,
   ],
