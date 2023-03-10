fada9bf392cc91b7961e49cd8e086c1ff9634a5f
Nikolay Nikolov <nikolay.nikolov@syncron.com>
2023-03-10 11:17:44 +0200

    add ngrx reducer

--- angular-guide/src/app/app.module.ts
+++ angular-guide/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { LoggingService } from './logging.service';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -16,6 +17,7 @@ import { LoggingService } from './logging.service';
     AppRouterModule,
     SharedModule,
     CoreModule,
+    StoreModule.forRoot({}, {}),
   ],
 
   bootstrap: [AppComponent],
